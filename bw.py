b0cbcc77aed62bf58755cfd2ac91473ff49c15ac
Jacob Hileman <jaycob@gmail.com>
2018-09-22 01:07:05 -0400

    clean up code and add comments

--- bw.py
+++ bw.py
@@ -12,10 +12,15 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 # create the dbox object for read/writing files
 dbx = dropbox.Dropbox(ACCESS_TOKEN)
 
+print("""
+******************************************
+Running the Speedtest script
+******************************************""")
 # run the speedtest script to get info
 response = subprocess.Popen(
     "speedtest-cli --simple",
     shell=True,
+    encoding="utf-8",
     stdout=subprocess.PIPE).stdout.read()
 
 # process the response so we just get the numbers
@@ -24,12 +29,15 @@ ping = response[0].replace("Ping: ", "").replace(" ms", "")
 download = response[1].replace("Download: ", "").replace(" Mbit/s", "")
 upload = response[2].replace("Upload: ", "").replace(" Mbit/s", "")
 
-##############################
+
+print("Ping: {}ms, Download: {} mbit/s, Upload: {} mbit/s \n\n".format(ping, download, upload))
+
+############################################################
 # download the csv file from dropbox.
 # it returns a tuple with two entries.
 # the first entry is metadata and the second is the fileobject
 # which has the content of the file
-##############################
+############################################################
 metadata, fileobject = dbx.files_download("/bw.csv")
 
 content = fileobject.content.decode()
