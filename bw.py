d9798e0efc878c8cdd206a5faf8e4005d72896a7
Jacob Hileman <jaycob@gmail.com>
2018-09-22 18:18:39 -0400

    add push notifications.. still in beta

--- bw.py
+++ bw.py
@@ -1,50 +1,97 @@
+import time
 import dropbox
 from dotenv import load_dotenv
 import os
 import subprocess
 import re
 import time
+import sys
+import push  # used to send pushover messages to my device
 
 # load .env vars which contains my token
 load_dotenv()
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
 
 # create the dbox object for read/writing files
-dbx = dropbox.Dropbox(ACCESS_TOKEN)
+dbx = dropbox.Dropbox(DROPBOX_TOKEN)
 
 print("""
 ******************************************
 Running the Speedtest script
 ******************************************""")
 
-# run the speedtest script to get info
-response = subprocess.Popen(
-    "speedtest-cli --simple",
-    shell=True,
-    encoding="utf-8",
-    stdout=subprocess.PIPE).stdout.read()
 
-# process the response so we just get the numbers
-print(response)
-response = response.splitlines()
-ping = response[0].replace("Ping: ", "").replace(" ms", "")
-download = response[1].replace("Download: ", "").replace(" Mbit/s", "")
-upload = response[2].replace("Upload: ", "").replace(" Mbit/s", "")
+def get_speed():
+    # run the speedtest script to get info
+    # response = subprocess.Popen(
+    #     "speedtest-cli --simple",
+    #     shell=True,
+    #     encoding="utf-8",
+    #     stdout=subprocess.PIPE).stdout.read()
+
+    response = """Ping: 3000.943 ms
+    Download: 40.93 Mbit/s
+    Upload: 2.33 Mbit/s"""
+
+    # process the response so we just get the numbers
+    # print(response)
+    response = response.splitlines()
+    ping = float(response[0].replace("Ping: ", "").replace(" ms", ""))
+    download = float(response[1].replace("Download: ", "").replace(" Mbit/s", ""))
+    upload = float(response[2].replace("Upload: ", "").replace(" Mbit/s", ""))
+
+    return(ping, download, upload)
 
 
-#print("Ping: {}ms, Download: {} mbit/s, Upload: {} mbit/s \n\n".format(ping, download, upload))
 
-############################################################
-# download the csv file from dropbox.
-# it returns a tuple with two entries.
-# the first entry is metadata and the second is the fileobject
-# which has the content of the file in bytes
-############################################################
-metadata, fileobject = dbx.files_download("/bw.csv")
-content = fileobject.content.decode()
-date, time = time.strftime("%m/%d/%y"), time.strftime("%H:%M")
 
-content = content + "\n" + date + "," + time + "," + ping + "," + download + "," + upload
-f = content.encode()
 
-dbx.files_upload(f, "/bw.csv", mode=dropbox.files.WriteMode("overwrite"))
+
+
+
+
+
+ping, download, upload = get_speed()
+x = 0
+push_msg_sent = False
+
+while ping > 3000:
+    print("ping is greater than 3 seconds!")
+    time.sleep(2)
+    ping, download, upload = get_speed()  
+
+    print("Ping: {}ms, Download: {} mbit/s, Upload: {} mbit/s \n\n".format(ping, download, upload))
+
+    if push_msg_sent is False:
+        push_msg = "Ping: {}ms, Download: {} mbit/s, Upload: {} mbit/s \n\n".format(ping, download, upload)
+        push.send_push_message(push_msg, "Slow broadband alert!")
+        push_msg_sent = True
+
+    ############################################################
+    # download the csv file from dropbox.
+    # it returns a tuple with two entries.
+    # the first entry is metadata and the second is the fileobject
+    # which has the content of the file in bytes
+    ############################################################
+    metadata, fileobject = dbx.files_download("/bw.csv")
+    content = fileobject.content.decode()
+    todays_date, current_time = time.strftime("%m/%d/%y"), time.strftime("%H:%M")
+
+    content = content + "\n" + todays_date + "," + current_time + "," + \
+                        str(ping) + "," + str(download) + "," + str(upload)
+    f = content.encode()
+
+    dbx.files_upload(f, "/bw.csv", mode=dropbox.files.WriteMode("overwrite"))
+    x += 1
+    if x > 2:
+        ping = 5
+        push_msg = "Normal speeds are back now!"
+        push.send_push_message(push_msg, "We're back to normal speed!")
+
+    
+    
+
+print("""
+*************************
+Script is complete!
+*************************""")
