e2f3b801233de54493b0d7c577324946f6c4124e
Jacob Hileman <jaycob@gmail.com>
2018-09-22 21:06:16 -0400

    add pushover support. move functions into the same doc.

--- bw.py
+++ bw.py
@@ -3,14 +3,65 @@ import dropbox
 from dotenv import load_dotenv
 import os
 import subprocess
-import re
 import time
-import sys
-import push  # used to send pushover messages to my device
+import json
+from pushover import Client
 
-# load .env vars which contains my token
+
+def get_speed():
+    # run the speedtest script to get info
+    response = subprocess.Popen(
+        "speedtest-cli --json",
+        shell=True,
+        encoding="utf-8",
+        stdout=subprocess.PIPE).stdout.read()
+
+    # process the response so we just get the numbers
+    response = json.loads(response)
+    ping, download, upload, server = response["ping"], response["download"], response["upload"], response["server"]["url"]
+
+    print("Ping: {}ms, Download: {:,} mbit/s, Upload: {:,} mbit/s, Server: {} \n\n".format(ping, download, upload, server))
+
+    return(int(ping), download, upload, server)
+
+
+
+def write_to_csv(ping, download, upload, server):
+    # download the csv from dropbox. dbx returns a tuple. 
+    # one for the meta data of the file and one of the file itself
+    metadata, fileobject = dbx.files_download("/bw.csv")  
+
+    content = fileobject.content.decode()  # open the content of the fileobject
+    todays_date, current_time = time.strftime("%m/%d/%y"), time.strftime("%H:%M")  # get todays date/time
+    
+    content = content + "\n" + str(todays_date) + "," + str(current_time) + "," + \
+                        str(ping) + "," + str(download) + "," + str(upload) + "," + str(server)  # write the new line
+    f = content.encode()  # encode it back to bytes
+
+    dbx.files_upload(f, "/bw.csv", mode=dropbox.files.WriteMode("overwrite"))  # upload it back to dropbox
+
+
+
+def send_push_message(msg, title):
+    push = Client(PUSHOVER_USER_TOKEN, api_token=PUSHOVER_API_TOKEN)
+    push.send_message(msg, title=title)
+
+
+
+
+
+############################################### 
+# initialize vars and get the script rolling!
+###############################################
+
+ # load .env vars which contains my token
 load_dotenv()
 DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
+PUSHOVER_USER_TOKEN = os.getenv("PUSHOVER_USER_TOKEN")
+PUSHOVER_API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
+
+max_ping = 18  # if ping in ms is over this that's bad
+secs_before_retry = 5  # number of seconds to wait before testing bandwidth again if it is greater than max_ping
 
 # create the dbox object for read/writing files
 dbx = dropbox.Dropbox(DROPBOX_TOKEN)
@@ -21,77 +72,34 @@ Running the Speedtest script
 ******************************************""")
 
 
-def get_speed():
-    # run the speedtest script to get info
-    # response = subprocess.Popen(
-    #     "speedtest-cli --simple",
-    #     shell=True,
-    #     encoding="utf-8",
-    #     stdout=subprocess.PIPE).stdout.read()
+# start the script by getting our speed!
+# get_speed() returns 4 separate vars
+ping, download, upload, server = get_speed()
 
-    response = """Ping: 3000.943 ms
-    Download: 40.93 Mbit/s
-    Upload: 2.33 Mbit/s"""
+write_to_csv(ping, download, upload, server)
 
-    # process the response so we just get the numbers
-    # print(response)
-    response = response.splitlines()
-    ping = float(response[0].replace("Ping: ", "").replace(" ms", ""))
-    download = float(response[1].replace("Download: ", "").replace(" Mbit/s", ""))
-    upload = float(response[2].replace("Upload: ", "").replace(" Mbit/s", ""))
+push_msg_sent = False  # default value for the initial push message
 
-    return(ping, download, upload)
-
-
-
-
-
-
-
-
-
-
-ping, download, upload = get_speed()
-x = 0
-push_msg_sent = False
-
-while ping > 3000:
-    print("ping is greater than 3 seconds!")
-    time.sleep(2)
-    ping, download, upload = get_speed()  
-
-    print("Ping: {}ms, Download: {} mbit/s, Upload: {} mbit/s \n\n".format(ping, download, upload))
+# this runs if the ping is above the setting
+while ping > max_ping:
+    print("Ping is greater than {}".format(max_ping))
+    time.sleep(secs_before_retry)
+    ping, download, upload, server = get_speed()  # try the broadband test again
 
     if push_msg_sent is False:
-        push_msg = "Ping: {}ms, Download: {} mbit/s, Upload: {} mbit/s \n\n".format(ping, download, upload)
-        push.send_push_message(push_msg, "Slow broadband alert!")
+        push_msg = "Ping: {}ms, Download: {:,} mbit/s, Upload: {:,} mbit/s, Server: {} \n\n".format(ping, download, upload, server)
+        send_push_message(push_msg, "Slow broadband alert!")
         push_msg_sent = True
 
-    ############################################################
-    # download the csv file from dropbox.
-    # it returns a tuple with two entries.
-    # the first entry is metadata and the second is the fileobject
-    # which has the content of the file in bytes
-    ############################################################
-    metadata, fileobject = dbx.files_download("/bw.csv")
-    content = fileobject.content.decode()
-    todays_date, current_time = time.strftime("%m/%d/%y"), time.strftime("%H:%M")
+    # once the ping is below our threshold send a notification saying everything is good.
+    if ping <= max_ping:
+        print("hey the ping is low again and this ran!")
+        send_push_message("Ping is now: {}".format(ping), "We're back to normal speed!")
 
-    content = content + "\n" + todays_date + "," + current_time + "," + \
-                        str(ping) + "," + str(download) + "," + str(upload)
-    f = content.encode()
+    write_to_csv(ping, download, upload, server)  # write the results to csv
 
-    dbx.files_upload(f, "/bw.csv", mode=dropbox.files.WriteMode("overwrite"))
-    x += 1
-    if x > 2:
-        ping = 5
-        push_msg = "Normal speeds are back now!"
-        push.send_push_message(push_msg, "We're back to normal speed!")
-
-    
-    
 
 print("""
-*************************
+******************************************
 Script is complete!
-*************************""")
+******************************************""")
