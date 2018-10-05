39e7177ba0fd4267eba8de55576726c0acaa745f
Jacob Hileman <jaycob@gmail.com>
2018-10-05 14:23:51 -0400

    add __main__ function.

--- bw.py
+++ bw.py
@@ -43,53 +43,53 @@ def send_push_message(msg, title):
     push.send_message(msg, title=title)
 
 
-###############################################
-# initialize vars and get the script rolling!
-###############################################
+if __name__ == "__main__":
+    ###############################################
+    # initialize vars and get the script rolling!
+    ###############################################
 
-# load .env vars which contains my token
-load_dotenv()
-DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
-PUSHOVER_USER_TOKEN = os.getenv("PUSHOVER_USER_TOKEN")
-PUSHOVER_API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
+    # load .env vars which contains my token
+    load_dotenv()
+    DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
+    PUSHOVER_USER_TOKEN = os.getenv("PUSHOVER_USER_TOKEN")
+    PUSHOVER_API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
 
-max_ping = 1000  # if ping in ms is over this that's bad
-secs_before_retry = 90  # number of seconds to wait before testing bandwidth again if it is greater than max_ping
+    max_ping = 1000  # if ping in ms is over this that's bad
+    secs_before_retry = 90  # number of seconds to wait before testing bandwidth again if it is greater than max_ping
+
+    print("""
+    ******************************************
+    Running the Speedtest script
+    ******************************************""")
+    dbx = dropbox.Dropbox(DROPBOX_TOKEN)   # create the dbox object for read/writing files
+    push = Client(PUSHOVER_USER_TOKEN, api_token=PUSHOVER_API_TOKEN)  # create the pushover object
+
+    # start the script by getting our speed!
+    ping, download, upload, server = get_speed()  # get_speed() returns 4 separate vars
+
+    write_to_csv(ping, download, upload, server)
+
+    push_msg_sent = False  # default value for the initial push message
+
+    # this runs if the ping is above the setting
+    while ping > max_ping:
+        print(f"Ping is greater than {max_ping}")
+        time.sleep(secs_before_retry)  # wait before re-trying to see if the connection is better
+        ping, download, upload, server = get_speed()  # try the broadband test again
+
+        if push_msg_sent is False:
+            push_msg = f"Ping: {ping}ms, Download: {download / 8000000:,} mb/s, Upload: {upload / 8000000:,} mb/s, Server: {server} \n\n"
+            send_push_message(push_msg, "Slow broadband alert!")
+            push_msg_sent = True
+
+        # once the ping is below our threshold send a notification saying everything is good.
+        if ping <= max_ping:
+            send_push_message(f"Ping is now: {ping}", "We're back to normal speed!")
+
+        write_to_csv(ping, download, upload, server)  # write the results to csv
 
 
-print("""
-******************************************
-Running the Speedtest script
-******************************************""")
-dbx = dropbox.Dropbox(DROPBOX_TOKEN)   # create the dbox object for read/writing files
-push = Client(PUSHOVER_USER_TOKEN, api_token=PUSHOVER_API_TOKEN)  # create the pushover object
-
-# start the script by getting our speed!
-ping, download, upload, server = get_speed()  # get_speed() returns 4 separate vars
-
-write_to_csv(ping, download, upload, server)
-
-push_msg_sent = False  # default value for the initial push message
-
-# this runs if the ping is above the setting
-while ping > max_ping:
-    print(f"Ping is greater than {max_ping}")
-    time.sleep(secs_before_retry)  # wait before re-trying to see if the connection is better
-    ping, download, upload, server = get_speed()  # try the broadband test again
-
-    if push_msg_sent is False:
-        push_msg = f"Ping: {ping}ms, Download: {download / 8000000:,} mb/s, Upload: {upload / 8000000:,} mb/s, Server: {server} \n\n"
-        send_push_message(push_msg, "Slow broadband alert!")
-        push_msg_sent = True
-
-    # once the ping is below our threshold send a notification saying everything is good.
-    if ping <= max_ping:
-        send_push_message(f"Ping is now: {ping}", "We're back to normal speed!")
-
-    write_to_csv(ping, download, upload, server)  # write the results to csv
-
-
-print("""
-******************************************
-Script is complete!
-******************************************""")
+    print("""
+    ******************************************
+    Script is complete!
+    ******************************************""")
